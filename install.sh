b8eade1465295f16676a0f7876d6c5cd462fca1c
WHaffmans <11960527+WHaffmans@users.noreply.github.com>
2019-04-08 10:33:04 +0200

    add Xresources

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ ln -s  ~/dotfiles/.gvimrc ~/.gvimrc
 ln -s  ~/dotfiles/.zshrc ~/.zshrc
 ln -s  ~/dotfiles/i3 ~/.config/i3/config
 ln -s  ~/dotfiles/polybar ~/.config/polybar/config
+ln -s  ~/dotfiles/Xresources ~/.Xresources
 
 echo "done"
 

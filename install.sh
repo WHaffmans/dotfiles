eb8252be68b28e0cb19b91a6bf39e5f15ffb3bcf
WHaffmans <11960527+WHaffmans@users.noreply.github.com>
2019-04-08 10:25:06 +0200

    add i3 and polybar

--- install.sh
+++ install.sh
@@ -3,6 +3,8 @@
 ln -s  ~/dotfiles/.vimrc ~/.vimrc
 ln -s  ~/dotfiles/.gvimrc ~/.gvimrc
 ln -s  ~/dotfiles/.zshrc ~/.zshrc
+ln -s  ~/dotfiles/i3 ~/.config/i3/config
+ln -s  ~/dotfiles/polybar ~/.config/polybar/config
 
 echo "done"
 
